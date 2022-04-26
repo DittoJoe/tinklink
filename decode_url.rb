1328fde45e4e9e7af20117f7c7d5aeb586297392
Joseph Rohde <70761811+DittoJoe@users.noreply.github.com>
2022-04-26 14:24:53 +0200

    refactoring map loop to one line

--- decode_url.rb
+++ decode_url.rb
@@ -2,8 +2,6 @@ require 'uri'
 
 puts "Please paste the Tink Link url:"
 split_params = gets.chomp.to_s.split(/\?/)[1].split(/&/)
-split_params.map! do |text|
-    text = URI.decode(text)
-end
+split_params.map! { |text| text = URI.decode(text) } 
 puts
 puts split_params
