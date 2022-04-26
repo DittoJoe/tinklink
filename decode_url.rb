cf97aefde57b6c1bb0ff0c7b7a57e85b7d82b701
Joseph Rohde <70761811+DittoJoe@users.noreply.github.com>
2022-04-26 14:19:53 +0200

    updating to decode all fields in case of scope

--- decode_url.rb
+++ decode_url.rb
@@ -2,6 +2,8 @@ require 'uri'
 
 puts "Please paste the Tink Link url:"
 split_params = gets.chomp.to_s.split(/\?/)[1].split(/&/)
-split_params[1] = URI.decode(split_params[1])
+split_params.map! do |text|
+    text = URI.decode(text)
+end
 puts
 puts split_params
